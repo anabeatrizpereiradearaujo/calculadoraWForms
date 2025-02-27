f2b88da002bd99b7a9d7e405d1f91ab24552e42b
amanda1304 <mand.amanda1304@gmail.com>
2025-02-27 17:35:54 -0300

    Calculadora WForms,

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -6,5 +6,25 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
