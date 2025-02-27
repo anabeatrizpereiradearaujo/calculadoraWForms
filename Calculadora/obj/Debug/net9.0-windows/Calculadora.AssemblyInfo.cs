644a4f4792bf583ba8b34b033369850d83691aff
amanda1304 <mand.amanda1304@gmail.com>
2025-02-27 17:03:20 -0300

    atualizar Branch

--- Calculadora/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3959b5f495c10e5fbfe0662f1524410e54c621a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ff9a67f007c79f1f962d628bbff63d5e1ec95f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
